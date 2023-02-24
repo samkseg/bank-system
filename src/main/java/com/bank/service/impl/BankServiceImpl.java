14f77358c8191e6d8d52aeaaa41e713f2f71ec5d
Ken <ken.segawa@iths.se>
2023-02-24 06:33:42 +0100

    Updated withdraw and deposit methods

--- src/main/java/com/bank/service/impl/BankServiceImpl.java
+++ src/main/java/com/bank/service/impl/BankServiceImpl.java
@@ -6,9 +6,9 @@ import com.bank.service.BankService;
 public class BankServiceImpl implements BankService {
     @Override
     public void withdraw(BankAccount bankAccount, double amount) {
-        if (amount != 0 && bankAccount.getBalance() >= amount) {
+        if (amount > 0 && bankAccount.getBalance() >= amount) {
             bankAccount.setBalance(bankAccount.getBalance() - amount);
-            bankAccount.setPrevTrans(bankAccount.getPrevTrans() - amount);
+            bankAccount.setPrevTrans(amount);
         } else if (bankAccount.getBalance() < amount) {
             System.out.println("Bank balance insufficient");
         }
@@ -16,7 +16,7 @@ public class BankServiceImpl implements BankService {
 
     @Override
     public void deposit(BankAccount bankAccount, double amount) {
-        if (amount != 0) {
+        if (amount > 0) {
             bankAccount.setBalance(bankAccount.getBalance() + amount);
             bankAccount.setPrevTrans(amount);
         }
