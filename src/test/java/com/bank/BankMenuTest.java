7e27a148b81363df8b19914a00b251fa3b8ba6cc
Ken <ken.segawa@iths.se>
2023-02-24 06:36:17 +0100

    Added BankMenuTest

---
+++ src/test/java/com/bank/BankMenuTest.java
@@ -0,0 +1,58 @@
+package com.bank;
+
+import com.bank.service.BankService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class BankMenuTest {
+
+    @Mock
+    private BankService bankService;
+    BankMenu bankMenu;
+    BankAccount bankAccount;
+
+    @BeforeEach
+    public void initialize() {
+        bankAccount = new BankAccount("Customer", "1");
+        bankAccount.setBalance(10000.0);
+    }
+
+    @Test
+    public void shouldDepositUsingInput() {
+        String userInput = "b" + System.getProperty("line.separator")
+                + "5000" + System.getProperty("line.separator")
+                + "a" + System.getProperty("line.separator")
+                + "e";
+        InputStream storedInputStream = (new ByteArrayInputStream(userInput.getBytes()));
+        System.setIn(storedInputStream);
+        bankMenu = new BankMenu(bankService,bankAccount);
+        bankMenu.menu();
+
+        verify(bankService, times(1)).deposit(bankAccount, 5000);
+    }
+
+    @Test
+    public void shouldWithdrawUsingInput() {
+        String userInput = "c" + System.getProperty("line.separator")
+                + "1000" + System.getProperty("line.separator")
+                + "a" + System.getProperty("line.separator")
+                + "e";
+        InputStream storedInputStream = (new ByteArrayInputStream(userInput.getBytes()));
+        System.setIn(storedInputStream);
+        bankMenu = new BankMenu(bankService, bankAccount);
+        bankMenu.menu();
+
+        verify(bankService, times(1)).withdraw(bankAccount, 1000);
+
+    }
+}
